be2fcfe4dfef3df708b3ed8b03b2e9bd2ba40b11
Justin Grevich <justin@grevi.ch>
2012-10-22 17:05:42 -0700

    initial reorganization for turning into a gem.

---
+++ ansible.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'Ansible'
+  s.version     = '0.0.1'
+  s.date        = '2012-10-22'
+  s.summary     = "Ruby-based home automation scripting framework for KNX and ZWave devices."
+  s.description = "A simple hello world gem"
+  s.authors     = ["Elias Karakoulakis (ekarak)", "Justin Grevich (jgrevich)"]
+  s.email       = ['elias.karakoulakis@gmail.com', 'justin@grevi.ch']
+  s.files       = ["lib/*.rb", "lib/**/*.rb"]
+  s.homepage    = 'https://github.com/jgrevich/ansible'
+  s.requirements << "KNX or ZWave controller + Devices"
+  s.requirements << "stompserver_ng"
+  s.requirements << "Thrift4OZW"
+end
---
+++ lib/ansible.rb
@@ -0,0 +1,48 @@
+=begin
+Project Ansible  - An extensible home automation scripting framework
+----------------------------------------------------
+Copyright (c) 2011 Elias Karakoulakis <elias.karakoulakis@gmail.com>
+
+SOFTWARE NOTICE AND LICENSE
+
+Project Ansible is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+Project Ansible is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Project Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+for more information on the LGPL, see:
+http://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License
+=end
+
+$:.push(Dir.getwd)
+$:.push(File.join(Dir.getwd, 'ansible'))
+$:.push(File.join(Dir.getwd, 'ansible/knx'))
+$:.push(File.join(Dir.getwd, 'ansible/zwave'))
+$:.push(File.join(Dir.getwd, 'ansible/devices'))
+
+require "rubygems"
+require "bundler/setup"
+require 'config'
+
+require 'transceiver'
+require 'zwave_transceiver'
+require 'zwave_command_classes'
+
+require 'knx_transceiver'
+require 'knx_tools'
+require 'knx_value'
+
+require 'ansible_device'
+
+
+include Ansible
+include Ansible::ZWave
+include Ansible::KNX
